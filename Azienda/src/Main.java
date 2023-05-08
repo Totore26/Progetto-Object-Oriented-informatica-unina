758d85f7f365adf289bb8edd7150877c6d9c7de2
Salvatore <tortora2611@gmail.com>
2023-05-08 12:39:19 +0200

    La volta buona che iniziamo

---
+++ Azienda/src/Main.java
@@ -0,0 +1,15 @@
+import CONTROLLER.Controller;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("GRANDE RAIMONDO!");
+
+        Controller c = new Controller();
+
+    }
+
+}
+
+
